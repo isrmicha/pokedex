52727ea245dce2d7b2b7700339d7c900579105d9
isrmicha <isrmicha@gmail.com>
2022-08-06 22:49:49 -0300

    chore: adding title and Favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import createEmotionServer from '@emotion/server/create-instance';
-import theme from '../src/theme';
-import createEmotionCache from '../src/createEmotionCache';
+import * as React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import createEmotionServer from "@emotion/server/create-instance";
+import theme from "../src/theme";
+import createEmotionCache from "../src/createEmotionCache";
 
 export default class MyDocument extends Document {
   render() {
@@ -18,6 +18,7 @@ export default class MyDocument extends Document {
           />
           <meta name="emotion-insertion-point" content="" />
           {(this.props as any).emotionStyleTags}
+          <title>Pokédex</title>
         </Head>
         <body>
           <Main />
@@ -74,7 +75,7 @@ MyDocument.getInitialProps = async (ctx) => {
   const emotionStyles = extractCriticalToChunks(initialProps.html);
   const emotionStyleTags = emotionStyles.styles.map((style) => (
     <style
-      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
       key={style.key}
       // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: style.css }}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import dynamic from "next/dynamic";
+import Head from "next/head";
 
 const Home = dynamic(() => import("../src/components/Home"), {
   ssr: false,
@@ -10,6 +11,10 @@ const Home = dynamic(() => import("../src/components/Home"), {
 const HomePage: NextPage = () => {
   return (
     <Container maxWidth="lg" disableGutters>
+      <Head>
+        <title>Pokedex</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Box>
         <Home />
       </Box>
