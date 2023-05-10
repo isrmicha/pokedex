e6da710f9e3b7d03d11cba28e24219222f053acb
Israel <isrmicha@gmail.com>
2023-05-10 01:46:35 -0300

    fix: last page bug

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 export const PAGE_SIZE = 10
 export const POKEAPI_ENDPOINT = "https://beta.pokeapi.co/graphql/v1beta";
-export const TOTAL_POKEMON_COUNT = 1281
+export const TOTAL_POKEMON_COUNT = 1270
