813876cd50334cf1c52e44adad6b94c50cb0c3ae
Israel <isrmicha@gmail.com>
2023-05-10 14:17:16 -0300

    feat: adding layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 import { type NextPage, } from "next"
 import Head from "next/head"
 import { signIn, signOut, useSession, } from "next-auth/react"
-import { Avatar, Button, Col, Row, Space, Spin, Tag, } from 'antd'
+import { Avatar, Button, Col, Row, Space, Tag, } from 'antd'
 import { Table, } from "~/components/table"
 import { Loading, } from "~/components/loading"
+import { Breadcrumb, Layout, Menu, theme, } from 'antd'
 
+const { Header, Content, Footer, } = Layout
 const Home: NextPage = () => {
+  const {
+    token: { colorBgContainer, },
+  } = theme.useToken()
   const { data: sessionData, status, } = useSession()
-  console.log(status)
   return (
     <>
       <Head>
@@ -15,39 +19,43 @@ const Home: NextPage = () => {
         <meta name="description" content="Develop by isrmicha" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-
-          <div className="flex flex-col items-center gap-2">
-
-            <div className="flex flex-col items-center justify-center gap-4">
-              <p className="text-center text-2xl text-white">
-
-                <Row justify={"end"}>
-                  <Col>
-                    <Space size={[12, 12,]} wrap>
-                      {status === 'loading' ? (<Loading />) : (
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+           <Row justify={"end"}>
+              <Col>
+                <Space size={[12, 12,]} wrap>
+                  {status === 'loading' ? (<Loading />) : (
+                    <>
+                      {sessionData && (
                         <>
-                          {sessionData && (
-                            <>
-                              <Avatar src={sessionData.user.image} alt="Rounded avatar" />
-                              <Tag>{sessionData.user?.name}</Tag>
-                            </>
-                          )}
-                          <Button type="primary" onClick={sessionData ? signOut : signIn}>
-                            {sessionData ? "Sign out" : "Sign in"}
-                          </Button>
+                          <Avatar src={sessionData.user.image} alt="Rounded avatar" />
+                          <Tag color="processing" >{sessionData.user?.name}</Tag>
                         </>
                       )}
-                    </Space>
-                  </Col>
-                </Row>
-              </p>
-              {sessionData && (<Table />)}
-            </div>
+                      <Button type="primary" onClick={sessionData ? signOut : signIn}>
+                        {sessionData ? "Sign out" : "Sign in"}
+                      </Button>
+                    </>
+                  )}
+                </Space>
+              </Col>
+            </Row>
+        </Header>
+        <Content style={{ padding: '0 50px', }}>
+          <Breadcrumb style={{ margin: '16px 0', }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content" style={{ background: colorBgContainer, }}>
+            {sessionData && (<Table />)}
           </div>
-        </div>
-      </main>
+        </Content>
+        <Footer style={{ textAlign: 'center', }}>Ant Design ©{new Date().getFullYear()} <a target="_blank" href="https://www.github.com/isrmicha">
+          @isrmicha
+        </a>
+        </Footer>
+      </Layout>
     </>
   )
 }
