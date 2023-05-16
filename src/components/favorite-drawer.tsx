db53106e44c43aefc28d128824d18ab926926647
Israel <isrmicha@gmail.com>
2023-05-16 00:32:38 -0300

    fix: sprites for favorites

--- src/components/favorite-drawer.tsx
+++ src/components/favorite-drawer.tsx
@@ -1,4 +1,6 @@
-import { Drawer, } from "antd"
+import { Drawer, Image, } from "antd"
+import { startCase } from "lodash"
+import { getPokemonImage } from "~/utils/image"
 import { trpc, } from "~/utils/trpc"
 
 export const FavoriteDrawer = ({ onClose, favoritedIds,}: any) => {
@@ -7,6 +9,6 @@ export const FavoriteDrawer = ({ onClose, favoritedIds,}: any) => {
     )
 
     return <Drawer title="Favorites" placement="right" open={true} onClose={() => onClose(false)}>
-        {pokemons?.items.map(({id, name,})=> <p key={id}>{id} - {name}</p>)}
+        {pokemons?.items.map(({id, name})=> <p key={id}><Image src={getPokemonImage(id)} width={25} height={25} preview={false} /> #{id} - {startCase(name)}</p>)}
     </Drawer>
 }
