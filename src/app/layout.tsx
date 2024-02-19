d2a3e3baa8d76aa81887fc9c2c57335e12a94db7
Israel <isrmicha@gmail.com>
2024-02-18 22:48:41 -0300

    chore: testing with cssBaseline

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { TRPCReactProvider } from "~/trpc/react";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { darkTheme } from "~/theme";
 import NextAuthProvider from "./contet/NextAuthProvider";
+import CssBaseline from '@mui/material/CssBaseline';
 
 export const metadata = {
   title: "Pokedex",
@@ -12,10 +13,11 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body >
+        <CssBaseline />
         <AppRouterCacheProvider>
           <ThemeProvider theme={darkTheme}>
             <TRPCReactProvider>
